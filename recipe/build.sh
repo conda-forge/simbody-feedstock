adf1dca529324fe740549ffc941f1b1a3649cb78
Christopher Dembia <cld72@cornell.edu>
2018-10-30 18:45:28 -0700

    Bring back GLUT_OVERRIDE

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,14 +14,16 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	# https://github.com/simbody/simbody/issues/400 for more details. Once
 	# that is figured out then this test should be enabled.
 	SKIP_TEST="-E TestCustomConstraints"
+	GLUT_OVERRIDE=(-DCMAKE_CXX_FLAGS="-I$PREFIX/include")
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	SKIP_TEST=()
+	GLUT_OVERRIDE=()
 fi
 cmake .. \
 	-DCMAKE_INSTALL_PREFIX="$PREFIX" \
 	-DCMAKE_INSTALL_LIBDIR="lib" \
 	-DCMAKE_BUILD_TYPE="RELEASE" \
-	-DBUILD_USING_OTHER_LAPACK="$PREFIX/lib/libopenblas${SHLIB_EXT}"
+	-DBUILD_USING_OTHER_LAPACK="$PREFIX/lib/libopenblas${SHLIB_EXT}" $GLUT_OVERRIDE
 make
 # NOTE: Run the tests here in the build directory to make sure things are built
 # correctly. This cannot be specified in the meta.yml:test section because it
