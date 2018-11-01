0eb1e9f289e5f00d6c389c9c172dd5645f936646
Christopher Dembia <cld72@cornell.edu>
2018-10-31 23:35:20 -0700

    Try cross-linux.cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -17,11 +17,17 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 	# The CMAKE_CXX_FLAGS is required due to this bug in Simbody:
 	# https://github.com/simbody/simbody/issues/511
 	GLUT_OVERRIDE=(-DCMAKE_CXX_FLAGS="-I$PREFIX/include")
+    CMAKE_PLATFORM_FLAGS=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 elif [[ "$OSTYPE" == "darwin"* ]]; then
 	SKIP_TEST=()
 	GLUT_OVERRIDE=()
+    CMAKE_PLATFORM_FLAGS=()
 fi
 
+# https://github.com/AnacondaRecipes/freeglut-feedstock/blob/master/recipe/build.sh
+
+echo "DEBUG PREFIX"
+echo "$PREFIX"
 ls $PREFIX/lib
 # cp /usr/lib64/libGL.so $PREFIX/lib
 cmake .. \
@@ -29,9 +35,10 @@ cmake .. \
 	-DCMAKE_INSTALL_LIBDIR="lib" \
 	-DCMAKE_BUILD_TYPE="RELEASE" \
     -DCMAKE_VERBOSE_MAKEFILE=on \
+    $CMAKE_PLATFORM_FLAGS \
 	-DBUILD_USING_OTHER_LAPACK="$PREFIX/lib/libopenblas${SHLIB_EXT}" $GLUT_OVERRIDE
 # make doxygen
-make --jobs ${CPU_COUNT}
+make --jobs ${CPU_COUNT} simbody-visualizer
 # NOTE: Run the tests here in the build directory to make sure things are built
 # correctly. This cannot be specified in the meta.yml:test section because it
 # won't be run in the build directory.
