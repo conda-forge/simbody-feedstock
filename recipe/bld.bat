ec8aca7df404d95967187ab504c77a7a04a44f5e
Isuru Fernando <isuruf@gmail.com>
2019-03-24 22:33:37 -0500

    Full paths to .lib

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,7 +3,7 @@ cd build
 cmake -G "%CMAKE_GENERATOR%" ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-  -DBUILD_USING_OTHER_LAPACK="lapack.lib;blas.lib" ^
+  -DBUILD_USING_OTHER_LAPACK="%LIBRARY_LIB%\lapack.lib;%LIBRARY_LIB%\blas.lib" ^
   ..
 cmake --build . --target doxygen --config Release
 cmake --build . --target install --config Release
