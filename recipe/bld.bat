bc5db16c4ca1f17fa29706196e3adc0a1897f2c0
Christopher Dembia <cld72@cornell.edu>
2020-05-24 08:46:46 -0700

    Upgrade to Simbody-3.7

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,15 +5,16 @@ cp %LIBRARY_LIB%\lapack.lib Platform\Windows\lib_x64\lapack.lib
 
 mkdir build
 cd build
-cmake -G "%CMAKE_GENERATOR%" ^
+cmake -G Ninja ^
+  -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
   -DBUILD_USING_OTHER_LAPACK="%LIBRARY_LIB%\lapack.lib;%LIBRARY_LIB%\blas.lib" ^
   ..
 
-cmake --build . --target doxygen --config Release
-cmake --build . --target install --config Release
+ninja doxygen
+ninja install
 REM NOTE: Run the tests here in the build directory to make sure things are
 REM built correctly. This cannot be specified in the meta.yml:test section
 REM because it won't be run in the build directory.
-ctest --build-config Release --output-on-failure
+ctest --output-on-failure
