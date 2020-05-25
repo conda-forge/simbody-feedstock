de6646d06a771507bc1b97e981a669023a744f44
Christopher Dembia <cld72@cornell.edu>
2020-05-25 12:27:03 -0700

    Use openblas.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,13 @@
 
+REM Do not overwrite Conda's libraries with Simbody's vendored libraries.
 del /f Platform\Windows\lib_x64\*.dll
 del /f Platform\Windows\lib_x64\*.lib
-copy %LIBRARY_LIB%\lapack.lib Platform\Windows\lib_x64\lapack.lib
-copy %LIBRARY_LIB%\blas.lib Platform\Windows\lib_x64\blas.lib
-echo "LIBRARY_LIB DEBUG"
-dir %LIBRARY_LIB%
-echo "LIBRARY_BIN DEBUG"
-dir %LIBRARY_BIN%
+REM copy %LIBRARY_LIB%\lapack.lib Platform\Windows\lib_x64\lapack.lib
+REM copy %LIBRARY_LIB%\blas.lib Platform\Windows\lib_x64\blas.lib
+REM echo "LIBRARY_LIB DEBUG"
+REM dir %LIBRARY_LIB%
+REM echo "LIBRARY_BIN DEBUG"
+REM dir %LIBRARY_BIN%
 
 mkdir build
 cd build
@@ -15,11 +16,11 @@ cmake -G Ninja .. -LAH ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-  -DBUILD_USING_OTHER_LAPACK="%LIBRARY_LIB%/lapack.lib;%LIBRARY_LIB%/blas.lib"
+  -DBUILD_USING_OTHER_LAPACK="%LIBRARY_LIB%/openblas.lib"
 
 ninja doxygen
 ninja
-REM NOTE: Run the tests here in the build directory to make sure things are
+REM Run the tests here in the build directory to make sure things are
 REM built correctly. This cannot be specified in the meta.yml:test section
 REM because it won't be run in the build directory.
 ctest --output-on-failure
