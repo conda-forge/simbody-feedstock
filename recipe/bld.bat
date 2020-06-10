fc7e23a3658d1091678feb342701c99b5c11aaae
Christopher Dembia <cld72@cornell.edu>
2020-06-10 15:21:27 -0700

    Fix openblas path on Windows.

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,12 +5,16 @@ del /f Platform\Windows\lib_x64\*.lib
 
 mkdir build
 cd build
+REM Convert back slashes to forward slashes, otherwise:
+REM (1) CMake will interpret the single back slashes as escapes
+REM (2) Conda will not properly replace absolute paths in SimbodyTargets-release.cmake.
+set "LIBRARY_LIB_FWD=%LIBRARY_LIB:\=/%"
 REM -LAH prints the values of all CMake variables.
 cmake -G Ninja .. -LAH ^
   -DCMAKE_BUILD_TYPE=Release ^
   -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
   -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX%" ^
-  -DBUILD_USING_OTHER_LAPACK="%LIBRARY_LIB%/openblas.lib"
+  -DBUILD_USING_OTHER_LAPACK="%LIBRARY_LIB_FWD%/openblas.lib"
 
 ninja doxygen
 ninja
