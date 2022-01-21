e5653947cb37ba208b87a0c411483882b6cd8f76
Tobias Fischer <info@tobiasfischer.info>
2022-01-21 14:04:38 +1000

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,16 +14,20 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 
 # -LAH prints the values of all CMake variables.
-cmake .. -LAH \
+cmake ${CMAKE_ARGS} .. -LAH \
     -DCMAKE_INSTALL_PREFIX="$PREFIX" \
     -DCMAKE_INSTALL_LIBDIR="lib" \
     -DCMAKE_BUILD_TYPE="Release" \
     -DBUILD_USING_OTHER_LAPACK="$PREFIX/lib/libblas${SHLIB_EXT};$PREFIX/lib/liblapack${SHLIB_EXT}"
 
-make doxygen
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  make doxygen
+fi
 make --jobs ${CPU_COUNT}
 # NOTE: Run the tests here in the build directory to make sure things are built
 # correctly. This cannot be specified in the meta.yml:test section because it
 # won't be run in the build directory.
-eval "ctest ${SKIP_TEST}"
+if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+  eval "ctest ${SKIP_TEST}"
+fi
 make install
